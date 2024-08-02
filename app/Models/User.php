fcde8445ab8eff629e63135578c51ab0af7c28c8
kadjivdev <kadjivdev@gmail.com>
2024-08-02 09:03:57 -0700

    push of today

---
+++ app/Models/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    //  protected $table = 'dbo.[User]';
+
+     protected $guarded = [];
+    
+    //  protected $fillable = [
+    //     'firstname',
+    //     'lastname',
+    //     "denomination",
+    //     'phone',
+    //     'email',
+    //     'password',
+    //     "ifu",
+    //     "rccm"
+    // ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        ##======== CREATION D'UN ADMIN PAR DEFAUT ============####
+        // $userData = [
+        //     'lastname' => 'SALIOU',
+        //     'firstname' => 'Augustin',
+        //     'email' => 'centredeformationabc@gmail.com',
+        //     'password' => 'admin', #gogo@1315
+        //     'phone' => "40544540",
+        // ];
+        \App\Models\User::factory(100)->create();
+    }
+}
